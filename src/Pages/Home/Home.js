e9ab88cf1e766b66cbd416e425f35fdb369d2a01
SEC-MASUM <masum.jess42@gmail.com>
2022-05-19 04:11:11 +0600

    build : use useQuery to fetch task

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,15 +1,27 @@
 import React from "react";
+import { useQuery } from "react-query";
 import InputForm from "./InputForm/InputForm";
 import Tasks from "./Tasks/Tasks";
 
 const Home = () => {
+  const {
+    data: tasks,
+    isLoading,
+    refetch,
+  } = useQuery("tasks", () =>
+    fetch("http://localhost:5000/task").then((res) => res.json())
+  );
+
+  if (isLoading) {
+    return <h1>Loading</h1>;
+  }
   return (
     <div className="container mx-auto">
       <h1 className="text-3xl text-center text-purple-600 underline font-bold uppercase">
         Add Your Task List
       </h1>
-      <InputForm></InputForm>
-      <Tasks />
+      <InputForm refetch={refetch}></InputForm>
+      <Tasks refetch={refetch} tasks={tasks} />
     </div>
   );
 };
