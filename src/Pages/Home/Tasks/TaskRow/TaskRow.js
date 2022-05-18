bcee334fea0a333ffb04e53b5ebe4c0dc0198397
SEC-MASUM <masum.jess42@gmail.com>
2022-05-19 04:15:10 +0600

    feat : implement Complete and Delete button functionality

--- src/Pages/Home/Tasks/TaskRow/TaskRow.js
+++ src/Pages/Home/Tasks/TaskRow/TaskRow.js
@@ -1,12 +1,66 @@
 import React from "react";
+import { toast } from "react-toastify";
 
-const TaskRow = () => {
+const TaskRow = ({ task, index, refetch }) => {
+  const { _id, title, description } = task;
+
+  const handleDelete = (id) => {
+    const url = `http://localhost:5000/task/${id}`;
+    fetch(url, {
+      method: "DELETE",
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.deletedCount) {
+          toast.success(`Task ${title} is deleted.`);
+          refetch();
+        }
+      });
+  };
+
+  const handleComplete = (id) => {
+    fetch(`http://localhost:5000/task/complete/${id}`, {
+      method: "PUT",
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        if (data.modifiedCount > 0) {
+          refetch();
+          toast.success(`Task: ${title} is complete.`);
+        }
+      });
+  };
+
   return (
     <tr>
-      <th></th>
-      <th>Title</th>
-      <th>Description</th>
-      <th>Action</th>
+      <th>{index + 1}</th>
+      <td>
+        <span className={task?.status === "complete" ? "line-through" : ""}>
+          {title}
+        </span>
+      </td>
+      <td>
+        <span className={task?.status === "complete" ? "line-through" : ""}>
+          {description}
+        </span>
+      </td>
+      <td>
+        <div className="space-x-2">
+          <button
+            onClick={() => handleComplete(_id)}
+            className="btn btn-sm btn-success"
+          >
+            Complete
+          </button>
+          <button
+            onClick={() => handleDelete(_id)}
+            className="btn btn-sm btn-error"
+          >
+            Delete
+          </button>
+        </div>
+      </td>
     </tr>
   );
 };
