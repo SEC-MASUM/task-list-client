55729c26c276b3981a1159df3cf470aab86df7a9
SEC-MASUM <masum.jess42@gmail.com>
2022-05-19 07:33:23 +0600

    style : add py-28

---
+++ src/Pages/Shared/Loading/Loading.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Loading = () => {
+  return (
+    <div className="py-28 flex justify-center items-center">
+      <button className="btn btn-secondary loading">Loading</button>
+    </div>
+  );
+};
+
+export default Loading;
