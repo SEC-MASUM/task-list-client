4bc72b02f53894c3ab7f7f5cbee57142b5b6a082
SEC-MASUM <masum.jess42@gmail.com>
2022-05-19 07:35:23 +0600

    feat : send user data to save in database and receive token

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,9 +1,66 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useSignInWithGoogle } from "react-firebase-hooks/auth";
+import Loading from "../Shared/Loading/Loading";
+import { useNavigate } from "react-router-dom";
+import auth from "../../Firebase/Firebase.init";
 
 const Login = () => {
+  const [signInWithGoogle, googleUser, googleLoading] =
+    useSignInWithGoogle(auth);
+  const [token, setToken] = useState("");
+
+  let navigate = useNavigate();
+
+  useEffect(() => {
+    if (token) {
+      navigate("/home");
+    }
+  }, [googleUser, navigate, token]);
+
+  if (googleUser) {
+    const email = googleUser?.user?.email;
+    const currentUser = { email: email };
+    console.log(email, currentUser);
+    if (email) {
+      const url = `http://localhost:5000/user/${email}`;
+      fetch(url, {
+        method: "PUT",
+        headers: {
+          "content-type": "application/json",
+        },
+        body: JSON.stringify(currentUser),
+      })
+        .then((res) => res.json())
+        .then((data) => {
+          console.log("data inside useToken", data);
+          const accessToken = data.accessToken;
+          localStorage.setItem("accessToken", accessToken);
+          setToken(accessToken);
+          
+        });
+    }
+  }
+
+  if (googleLoading) {
+    return <Loading />;
+  }
+
   return (
-    <div>
-      <h1>Login</h1>
+    <div className="flex flex-col min-h-screen justify-center items-center space-y-10">
+      <h2 className="text-center text-3xl text-purple-700 font-bold">
+        Task List
+      </h2>
+      <div className="card w-96 bg-base-100 shadow-xl">
+        <div className="card-body">
+          {/* Social Login */}
+          <button
+            onClick={() => signInWithGoogle()}
+            className="btn btn-outline"
+          >
+            Continue with Google
+          </button>
+        </div>
+      </div>
     </div>
   );
 };
